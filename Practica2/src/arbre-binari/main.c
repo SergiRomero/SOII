d3b6d0a4499a975ca0351fbffd2bbf0a2ce4519a
SERGI ROMERO GRIS <sromergr19.alumnes@ub052229.mat.ub.edu>
2019-10-11 18:46:56 +0200

    Falta contar ', recorrer tabla de ficheros

--- Practica2/src/arbre-binari/main.c
+++ Practica2/src/arbre-binari/main.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>  
 
 #include "red-black-tree.h"
 
@@ -25,6 +26,70 @@
  *
  */
 
+int process_line(char *line, rb_tree *tree)
+{
+    
+    node_data *n_data;
+    int i, j, is_word, len_line, ct;
+    char paraula[MAXCHAR];
+    
+    ct = 0;
+    i = 0;
+
+    len_line = strlen(line);
+
+    /* Search for the beginning of a candidate word */
+
+    while ((i < len_line) && (isspace(line[i]) || (ispunct(line[i])))) i++; 
+
+    /* This is the main loop that extracts all the words */
+
+    while (i < len_line)
+    {
+        j = 0;
+        is_word = 1;
+
+        /* Extract the candidate word including digits if they are present */
+
+        do {
+
+            if (isalpha(line[i])) // '
+                paraula[j] = line[i];
+            else 
+                is_word = 0;
+
+            j++; i++;
+
+            /* Check if we arrive to an end of word: space or punctuation character */
+
+        } while ((i < len_line) && (!isspace(line[i])) && (!ispunct(line[i])));
+
+        /* If word insert in list */
+
+        if (is_word) {
+
+            /* Put a '\0' (end-of-word) at the end of the string*/
+            paraula[j] = 0;
+
+            /* Print found word */
+            n_data = find_node(tree, paraula);
+    
+            if (n_data) {
+                n_data->num_times++;
+                printf("La paraula %s apareix %d cops a l'arbre.\n", paraula, n_data->num_times);
+                ct += 1;
+            }
+        }
+
+        /* Search for the beginning of a candidate word */
+
+        while ((i < len_line) && (isspace(line[i]) || (ispunct(line[i])))) i++; 
+
+    } /* while (i < len_line) */
+    
+    return ct;
+}
+
 int main(int argc, char **argv)
 {
   int ct;
@@ -37,7 +102,7 @@ int main(int argc, char **argv)
 
   if (argc != 2)
   {
-    printf("Usage: %s maxnum\n", argv[0]);
+    printf("Usage: %s file\n", argv[0]);
     exit(1);
   }
   
@@ -59,16 +124,14 @@ int main(int argc, char **argv)
 
   char *a;
   
-  printf("Read Doc\n");
+  printf("Read DICTIONARY\n");
   while (fgets(word, MAXCHAR, fp)) { //POR CADA PALABRA DE DICCIONARI
 
     /* Search if the key is in the tree */
     n_data = find_node(tree, word); 
 
     if (n_data != NULL) {
-
-      /* If the key is in the tree increment 'num' */
-      n_data->num_times++;
+      //YA ESTA AÑADIDO
     } else {
 
       /* If the key is not in the tree, allocate memory for the data
@@ -85,7 +148,7 @@ int main(int argc, char **argv)
       n_data->key = a;
       
       /* This is additional information that is stored in the tree */
-      n_data->num_times = 1;
+      n_data->num_times = 0;
 
       /* We insert the node in the tree */
       insert_node(tree, n_data);
@@ -95,11 +158,28 @@ int main(int argc, char **argv)
   
   /* We now dump the information of the tree to screen */
 
-  
-  fclose(fp);
-  
   ct = 0;
+  fclose(fp);
+    char line[MAXCHAR];
+    
+    if (argc != 2) {
+        printf("%s <file>\n", argv[0]);
+        exit(1);
+    }
+    
+    fp = fopen(argv[1], "r");
+    if (!fp) {
+        printf("Could not open file\n");
+        exit(1);
+    }
+    
+    while (fgets(line, MAXCHAR, fp))
+        ct += process_line(line, tree);
+    
+    fclose(fp);
   
+  
+  /*
   fp = fopen("words", "r");
   while (fgets(word, MAXCHAR, fp))
   {
@@ -112,13 +192,14 @@ int main(int argc, char **argv)
       ct += n_data->num_times;
     }
   }
-
-  printf("Nombre total que vegades que s'ha accedit a l'arbre: %d\n", ct);
-  fclose(fp);
+  fclose(fp);*/
   /* Delete the tree */
+  
+  printf("Nombre total que vegades que s'ha accedit a l'arbre: %d\n", ct);
   delete_tree(tree);
   free(tree);
 
   return 0;
 }
+
 
